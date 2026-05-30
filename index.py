93a1ca33345e45e5bf2e2a3401d04e3057fc5b53
zreechxnn <hoshikochan93@gmail.com>
2026-05-30 18:57:46 +0700

    yyy

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
 from app import create_app
+import sys, traceback
 
-app = create_app()
+try:
+    app = create_app()
+except Exception as e:
+    # Tampilkan error langsung saat diakses
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route('/')
+    def error():
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        tb_str = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
+        return f"<pre>{tb_str}</pre>", 500
